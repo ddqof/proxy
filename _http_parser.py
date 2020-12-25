24088b626b6a4eb6f4a689dbd03c17b088a856a7
ddqof <dmitry.podaruev@urfu.me>
2020-12-25 20:19:41 +0500

    Refactor _http_parser.py
    -Add docstrings
    -Now all extracting methods based on regex

--- _http_parser.py
+++ _http_parser.py
@@ -1,48 +1,89 @@
 import re
+from collections import namedtuple
 
-
-class Request:
-
-    def __init__(self, method: str, headers: dict, url: str, raw: bytes):
-        self.method = method
-        self.headers = headers
-        self.url = url
-        self.raw = raw
-        if method == "CONNECT":
-            self.port = 443
-        else:
-            try:
-                self.port = self.headers["Host"].split(":")[1]
-            except IndexError:
-                self.port = 80
-
-    def __str__(self):
-        return f"Request {self.method} {self.url}"
+method_regex = re.compile(r"^(\w+)")
+url_regex = re.compile(r"\w+ (.+?) HTTP/\d.\d")
+port_regex = re.compile(r":(\d+)$")
 
 
 def parse(http_meta: bytes):
+    """
+    Parse HTTP or HTTPS raw binary request body into.
+
+    Params:
+        http_meta: raw binary HTTP or HTTPS request body
+
+    Returns:
+        |namedtuple| Request("method", "headers", "url", "raw", "port").
+         "method": HTTP request method.
+         "headers": dict-mapping from HTTP request headers fields to
+          theirs value.
+         "url": absolute url requested in HTTP request. May also
+          include port at the end of url.
+         "raw": raw binary request body.
+         "port": HTTP request port
+    """
     decoded_data = http_meta.decode()
+    r_method = extract_method(decoded_data)
+    r_headers = build_headers(decoded_data)
+    r_url = extract_url(decoded_data)
+    r_port = extract_port(decoded_data)
+    Request = namedtuple(
+        "Request",
+        ["method", "headers", "url", "raw", "port"]
+    )
+    if r_port is None:
+        if r_method == "CONNECT":
+            r_port = 443
+        else:
+            r_port = 80
     return Request(
-        extract_method(decoded_data),
-        build_headers(decoded_data),
-        extract_url(decoded_data),
-        http_meta
+        method=r_method,
+        headers=r_headers,
+        url=r_url,
+        raw=http_meta,
+        port=r_port
     )
 
 
-def extract_method(http_meta: str):
-    return http_meta.split()[0]
+def extract_method(http_meta: str) -> str:
+    """
+    Extracts and returns method from HTTP request body.
+
+    Params:
+        http_meta: raw HTTP request body
+    """
+    return re.search(method_regex, http_meta).group(1)
 
 
-def extract_url(http_meta: str):
-    return re.search(
-        r"\w+ (.+?) HTTP/\d.\d",
-        http_meta,
-        re.DOTALL
-    ).group(1)
+def extract_url(http_meta: str) -> str:
+    """
+    Extracts and returns url from HTTP request body.
+
+    Params:
+        http_meta: raw HTTP request body
+    """
+    return re.search(url_regex, http_meta, re.DOTALL).group(1)
 
 
-def build_headers(http_meta: str):
+def extract_port(http_url: str) -> str:
+    """
+    Extracts and returns port from HTTP request body.
+
+    Params:
+        http_meta: raw HTTP request body
+    """
+    return re.search(url_regex, http_url).group(1)
+
+
+def build_headers(http_meta: str) -> dict:
+    """
+    Extracts and parses HTTP headers into dict-mapping
+    from headers fields in theirs values.
+
+    Params:
+        http_meta: raw HTTP request body
+    """
     #  removing first line with method
     h_lines = http_meta.split("\r\n")[1:]
     headers = {}
