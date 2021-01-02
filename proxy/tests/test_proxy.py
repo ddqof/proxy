a8a15f2e276a5314a609cba7951888d5a839b09e
ddqof <dmitry.podaruev@urfu.me>
2021-01-02 18:01:55 +0500

    separate test args line-by-line

--- proxy/tests/test_proxy.py
+++ proxy/tests/test_proxy.py
@@ -29,7 +29,10 @@ async def setup_server(port: int):
         await server.serve_forever()
 
 
-async def handle(reader: StreamReader, writer: StreamWriter):
+async def handle(
+        reader: StreamReader,
+        writer: StreamWriter
+):
     data = await reader.read(1024)
     if data.startswith(b"GET /"):
         writer.write(b"response for HTTP request")
@@ -38,32 +41,13 @@ async def handle(reader: StreamReader, writer: StreamWriter):
     await writer.wait_closed()
 
 
-async def send_then_recv(proxy_port: int, proxy_task: asyncio.Task, server_task: asyncio.Task):
-    await asyncio.sleep(0.01)  # time to complete setting up servers
-    cl_reader, cl_writer = await asyncio.open_connection(LOCALHOST, proxy_port)
-    cl_writer.write(b"some message")
-    try:
-        return await cl_reader.read(1024)
-    finally:
-        proxy_task.cancel()
-        server_task.cancel()
-
-
-async def run_test(proxy_port: int, server_port: int, test_case: Callable):
-    setup_proxy_task = asyncio.create_task(setup_proxy(proxy_port))
-    setup_remote_server_task = asyncio.create_task(setup_server(server_port))
-    testcase_task = asyncio.create_task(test_case(proxy_port, setup_proxy_task, setup_remote_server_task))
-    try:
-        await setup_proxy_task
-        await setup_remote_server_task
-        await testcase_task
-    except asyncio.CancelledError:
-        return testcase_task.result()
-
-
 @pytest.mark.asyncio
 @patch("proxy.proxy.parse")
-async def test_http_single_request(parser_mock, proxy_port, server_port):
+async def test_http_single_request(
+        parser_mock,
+        proxy_port,
+        server_port
+):
     parser_mock.return_value = Request(
         method="GET",
         abs_url=f"localhost:{server_port}",
@@ -78,7 +62,11 @@ async def test_http_single_request(parser_mock, proxy_port, server_port):
 
 @pytest.mark.asyncio
 @patch("proxy.proxy.parse")
-async def test_https_single_request(parser_mock, proxy_port, server_port):
+async def test_https_single_request(
+        parser_mock,
+        proxy_port,
+        server_port
+):
     parser_mock.return_value = Request(
         method="CONNECT",
         abs_url=f"localhost:{server_port}",
@@ -89,3 +77,40 @@ async def test_https_single_request(parser_mock, proxy_port, server_port):
 
     result = await run_test(proxy_port, server_port, send_then_recv)
     assert result == b"HTTP/1.1 200 Connection established\r\n\r\n"
+
+
+async def send_then_recv(
+        proxy_port: int,
+        proxy_task: asyncio.Task,
+        server_task: asyncio.Task
+):
+    await asyncio.sleep(0.01)  # time to complete setting up servers
+    cl_reader, cl_writer = await asyncio.open_connection(LOCALHOST, proxy_port)
+    cl_writer.write(b"some message")
+    try:
+        return await cl_reader.read(1024)
+    finally:
+        proxy_task.cancel()
+        server_task.cancel()
+
+
+async def run_test(
+        proxy_port: int,
+        server_port: int,
+        test_case: Callable
+):
+    setup_proxy_task = asyncio.create_task(
+        setup_proxy(proxy_port)
+    )
+    setup_remote_server_task = asyncio.create_task(
+        setup_server(server_port)
+    )
+    testcase_task = asyncio.create_task(
+        test_case(proxy_port,  setup_proxy_task, setup_remote_server_task)
+    )
+    try:
+        await setup_proxy_task
+        await setup_remote_server_task
+        await testcase_task
+    except asyncio.CancelledError:
+        return testcase_task.result()
