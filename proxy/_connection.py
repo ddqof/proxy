8f861e84ce6fca3bbab0b4ff6601d9322b72e7e0
ddqof <ddqof.vvv@gmail.com>
2021-05-22 12:31:52 +0500

    Move all connection-related methods from `Proxy` class to `Connection`

---
+++ proxy/_connection.py
@@ -0,0 +1,106 @@
+import logging
+
+from proxy._defaults import BLACK_HOLE_MSG, BLOCKED_WEBPAGE
+from proxy._endpoint import Endpoint
+from proxy._proxy_request import ProxyRequest, HTTPScheme
+from proxy.enpoint_type import EndpointType
+
+CHUNK_SIZE = 2 ** 20
+IMG_EXTS = {".jpg", ".jpeg", ".png"}
+HTTP_RESET_MSG = b"HTTP/1.1 403"
+
+LOGGER = logging.getLogger("proxy.proxy")
+
+
+class Connection:
+
+    def __init__(
+            self,
+            client_endpoint: Endpoint,
+            server_endpoint: Endpoint,
+            pr: ProxyRequest,
+            block_images: bool
+    ):
+        self.client = client_endpoint
+        self.server = server_endpoint
+        self.pr = pr
+        self.block_images = block_images
+
+    async def forward_to_server(self) -> None:
+        while True:
+            data = await self.client.read(CHUNK_SIZE)
+            if not data:
+                await self.server.close()
+                break
+            elif self.block_images:
+                try:
+                    decoded_data = data.decode()
+                    if (x in decoded_data for x in IMG_EXTS):
+                        await self.client.write_and_drain(HTTP_RESET_MSG)
+                        await self.client.close()
+                        return
+                except UnicodeDecodeError:
+                    pass
+            await self.server.write_and_drain(data)
+            self._log_forwarding(EndpointType.SERVER, data)
+
+    async def forward_to_client(self, spent: dict) -> None:
+        """
+        Receives data from remote server and forward it to localhost.
+        """
+        while True:
+            data = await self.server.read(CHUNK_SIZE)
+            restriction = self.pr.restriction
+            if data:
+                if restriction:
+                    initiator = restriction.initiator
+                    if spent[initiator] >= restriction.data_limit:
+                        await self._handle_limited_page()
+                        break
+                    spent[initiator] += len(data)
+                    LOGGER.debug(f"{spent[initiator]} SPENT FOR {initiator}")
+                await self.client.write_and_drain(data)
+                self._log_forwarding(EndpointType.CLIENT, data)
+            else:
+                await self.client.close()
+                break
+
+    async def _handle_limited_page(self) -> None:
+        """
+        Handles connection for restricted webpage.
+        In HTTP case it sends HTML notification page.
+        In HTTPS case it closes connection.
+        """
+        rsc = self.pr.restriction
+        if rsc.data_limit == 0:
+            LOGGER.info(BLACK_HOLE_MSG.format(url=rsc.initiator))
+        else:
+            LOGGER.info(BLOCKED_WEBPAGE.format(url=rsc.initiator))
+        if self.pr.scheme is HTTPScheme.HTTPS:
+            msg = "HTTP/1.1 403\r\n\r\n"
+        else:
+            msg = f"HTTP/1.1 200 OK\r\n\r\n{rsc.http_content}"
+        await self.client.write_and_drain(msg.encode())
+        await self.client.close()
+
+    async def reset(self):
+        LOGGER.info(f"Blocked image {self.pr.method:<{len('CONNECT')}} "
+                    f"{self.pr.abs_url}")
+        await self.client.write_and_drain(HTTP_RESET_MSG)
+        await self.client.close()
+
+    def _log_forwarding(self, endpoint_type: EndpointType, data: bytes) -> None:
+        """
+        Logging forwarding message.
+        """
+        query = f"{self.pr.method} {self.pr.abs_url}"
+        if endpoint_type is EndpointType.SERVER:
+            sender_ip = self.server.writer.get_extra_info("peername")[0]
+        else:
+            sender_ip = self.client.writer.get_extra_info("peername")[0]
+        if sender_ip == "::1":
+            pass
+        else:
+            query = "Response from server"
+        LOGGER.debug(f"{sender_ip:<{len('255.255.255.255')}} "
+                     f"{query} {len(data)}")
--- proxy/proxy.py
+++ proxy/proxy.py
@@ -5,24 +5,23 @@ from asyncio import StreamWriter, StreamReader
 from contextvars import ContextVar
 from itertools import chain
 
+from proxy._connection import CHUNK_SIZE
+from proxy._connection import Connection
 from proxy._defaults import (LOCALHOST,
-                             CHUNK_SIZE,
                              CONNECTION_ESTABLISHED_MSG,
                              HANDLING_HTTP_REQUEST_MSG,
                              HANDLING_HTTPS_CONNECTION_MSG,
                              CONNECTION_REFUSED_MSG,
                              START_SERVER_MSG,
-                             CONNECTION_CLOSED_MSG,
-                             BLOCKED_WEBPAGE,
-                             BLACK_HOLE_MSG)
+                             CONNECTION_CLOSED_MSG)
+from proxy._endpoint import Endpoint
 from proxy._log_config import LOGGING_CONFIG
 from proxy._proxy_request import ProxyRequest, HTTPScheme
-from proxy.connection import Connection
-from proxy.endpoint import Endpoint
-from proxy.enpoint_type import EndpointType
 
 logging.config.dictConfig(LOGGING_CONFIG)
 LOGGER = logging.getLogger(__name__)
+
+CONNECTION_ESTABLISHED_HTTP_MSG = b"HTTP/1.1 200 Connection established\r\n\r\n"
 
 
 class ProxyServer:
@@ -30,7 +29,6 @@ class ProxyServer:
     def __init__(self, port: int, block_images: bool = False, cfg=None):
         self.connection = ContextVar("connection")
         self.block_images = block_images
-        self.HTTP_RESET_MSG = b"HTTP/1.1 403"
         self.port = port
         self._spent_data = {}
         if cfg is not None:
@@ -41,7 +39,6 @@ class ProxyServer:
             for rsc in chain(cfg["limited"], cfg["black-list"]):
                 self._spent_data[rsc] = 0
         self.context_token = None
-        self.jpg_extensions = {".jpg", ".jpeg", ".png"}
 
     async def run(self):
         """
@@ -55,17 +52,6 @@ class ProxyServer:
 
         async with srv:
             await srv.serve_forever()
-
-    async def _reset_connection(
-            self,
-            client_writer: StreamWriter,
-            pr: ProxyRequest
-    ):
-        LOGGER.info(f"Blocked image {pr.method:<{len('CONNECT')}} "
-                    f"{pr.abs_url}")
-        client_writer.write(self.HTTP_RESET_MSG)
-        await client_writer.drain()
-        client_writer.close()
 
     async def _handle_connection(
             self,
@@ -82,9 +68,6 @@ class ProxyServer:
             if not raw_request:
                 return
             pr = ProxyRequest(raw_request, self._cfg)
-            if self.block_images and pr.is_image_request:
-                await self._reset_connection(client_writer, pr)
-                return
             LOGGER.info(f"{pr.method:<{len('CONNECT')}} "
                         f"{pr.abs_url}")
             try:
@@ -96,20 +79,23 @@ class ProxyServer:
                 return
             client_endpoint = Endpoint(client_reader, client_writer)
             server_endpoint = Endpoint(server_reader, server_writer)
-            self.context_token = self.connection.set(Connection(client_endpoint, server_endpoint, pr))
+            connection = Connection(client_endpoint, server_endpoint, pr, self.block_images)
+            self.context_token = self.connection.set(connection)
+            if self.block_images and pr.is_image_request:
+                await self.connection.get().reset()
+                return
             if pr.scheme is HTTPScheme.HTTPS:
                 await self._handle_https()
             else:
                 await self._handle_http()
-        except ConnectionResetError:
-            LOGGER.info(CONNECTION_CLOSED_MSG.format(url=pr.abs_url))
-            if self.context_token is not None:
-                self.connection.reset(self.context_token)
         except Exception as e:
-            LOGGER.exception(e)
+            if isinstance(e, ConnectionResetError):
+                LOGGER.info(CONNECTION_CLOSED_MSG.format(url=pr.abs_url))
+            else:
+                LOGGER.exception(e)
+                asyncio.get_event_loop().stop()
             if self.context_token is not None:
                 self.connection.reset(self.context_token)
-            asyncio.get_event_loop().stop()
 
     async def _handle_http(self) -> None:
         """
@@ -119,8 +105,8 @@ class ProxyServer:
         LOGGER.debug(HANDLING_HTTP_REQUEST_MSG.format(
             method=connection.pr.method, url=connection.pr.abs_url)
         )
-        await connection.server.write(connection.pr.raw)
-        await asyncio.gather(self._forward_to_local(), self._forward_to_remote())
+        await connection.server.write_and_drain(connection.pr.raw)
+        await asyncio.gather(connection.forward_to_client(self._spent_data), connection.forward_to_server())
 
     async def _handle_https(self) -> None:
         """
@@ -128,89 +114,6 @@ class ProxyServer:
         """
         connection = self.connection.get()
         LOGGER.debug(HANDLING_HTTPS_CONNECTION_MSG.format(url=connection.pr.hostname))
-        await connection.client.write(b"HTTP/1.1 200 Connection established\r\n\r\n")
+        await connection.client.write_and_drain(CONNECTION_ESTABLISHED_HTTP_MSG)
         LOGGER.debug(CONNECTION_ESTABLISHED_MSG.format(url=connection.pr.abs_url))
-        await asyncio.gather(self._forward_to_remote(), self._forward_to_local())
-
-    async def _forward_to_remote(self) -> None:
-        """
-        Receives data from localhost and forward it to server.
-        """
-        connection = self.connection.get()
-        while True:
-            data = await connection.client.read(CHUNK_SIZE)
-            if not data:
-                await connection.server.close()
-                break
-            elif self.block_images:
-                try:
-                    decoded_data = data.decode()
-                    if (x in decoded_data for x in self.jpg_extensions):
-                        await connection.client.write(self.HTTP_RESET_MSG)
-                        await connection.client.close()
-                        return
-                except UnicodeDecodeError:
-                    pass
-            await connection.server.write(data)
-            self._log_forwarding(EndpointType.SERVER, data)
-
-    async def _forward_to_local(self) -> None:
-        """
-        Receives data from remote server and forward it to localhost.
-        """
-        connection = self.connection.get()
-        while True:
-            data = await connection.server.read(CHUNK_SIZE)
-            if connection.pr.restriction:
-                if self._spent_data[connection.pr.restriction.initiator] >= \
-                        connection.pr.restriction.data_limit:
-                    await self._handle_limited_page()
-                    break
-            if data:
-                await connection.client.write(data)
-                self._log_forwarding(EndpointType.CLIENT, data)
-            else:
-                await connection.client.close()
-                break
-            if connection.pr.restriction:
-                self._spent_data[connection.pr.restriction.initiator] += len(data)
-                LOGGER.debug(
-                    f"{self._spent_data[connection.pr.restriction.initiator]}"
-                    f" WAS SPENT FOR {connection.pr.restriction.initiator}"
-                )
-
-    async def _handle_limited_page(self) -> None:
-        """
-        Handles connection for restricted webpage.
-        In HTTP case it sends HTML notification page.
-        In HTTPS case it closes connection.
-        """
-        connection = self.connection.get()
-        rsc = connection.pr.restriction
-        if rsc.data_limit == 0:
-            LOGGER.info(BLACK_HOLE_MSG.format(url=rsc.initiator))
-        else:
-            LOGGER.info(BLOCKED_WEBPAGE.format(url=rsc.initiator))
-        if connection.pr.scheme is HTTPScheme.HTTPS:
-            msg = "HTTP/1.1 403\r\n\r\n"
-        else:
-            msg = f"HTTP/1.1 200 OK\r\n\r\n{rsc.http_content}"
-        await connection.client.write(msg.encode())
-        await connection.client.close()
-
-    def _log_forwarding(self, endpoint_type: EndpointType, data: bytes) -> None:
-        """
-        Logging forwarding message.
-        """
-        connection = self.connection.get()
-        query = f"{connection.pr.method} {connection.pr.abs_url}"
-        if endpoint_type is EndpointType.SERVER:
-            sender_ip = connection.server.writer.get_extra_info("peername")[0]
-        else:
-            sender_ip = connection.client.writer.get_extra_info("peername")[0]
-        if sender_ip == "::1":
-            pass
-        else:
-            query = "Response from server"
-        LOGGER.debug(f"{sender_ip:<{len('255.255.255.255')}} "
-                     f"{query} {len(data)}")
+        await asyncio.gather(connection.forward_to_server(), connection.forward_to_client(self._spent_data))
